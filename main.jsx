4abb3953cc0d87c06226969ef66383a583330bae
Numart12 <dellenstudio@gmail.com>
2025-04-01 23:38:20 +0200

    Delete main.jsx

--- main.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-const App = () => {
-  return <h1>Hello from PraisonAI 👋</h1>;
-};
-
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
