d166e850028e5101db7ed464e39cf88252d938be
Numart12 <dellenstudio@gmail.com>
2025-04-01 23:52:10 +0200

    Create App.jsx

---
+++ src/praisonai-ui/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>👋 Вітаем у PraisonAI</h1>
+      <p>Frontend працуе! Цяпер можна падключаць CrewAI 🙌</p>
+    </div>
+  );
+}
