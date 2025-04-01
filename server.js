e870c2f07e5f2f3566006d1d934b5805c7ca1454
Numart12 <dellenstudio@gmail.com>
2025-04-01 23:32:00 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-// server.js
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.resolve(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, "dist")));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 app.listen(port, () => {
-  console.log(`🚀 Server is running on http://localhost:${port}`);
+  console.log(`Frontend running on port ${port}`);
 });
