ee3d3cb98c45496c31c043bbee3ed5bed65415cc
Numart12 <dellenstudio@gmail.com>
2025-04-01 23:49:03 +0200

    Update main.jsx

--- src/praisonai-ui/main.jsx
+++ src/praisonai-ui/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx"; // або іншы галоўны кампанент
-import "./index.css"; // калі маеш стылі
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
